04544eb1b9ad864b973b3ad8bc0b44aa8f0f3ea5
ycxpmmwnz2-create <ycxpmmwnz2@privaterelay.appleid.com>
2026-06-22 18:18:50 -0600

    Restore working Morgan Bank and save Firebase setup

---
+++ src/firebase/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC-96VLdKfwtQ-WaFT6BA2q1WLnk8hDe1A",
+  authDomain: "morgan-bank.firebaseapp.com",
+  projectId: "morgan-bank",
+  storageBucket: "morgan-bank.firebasestorage.app",
+  messagingSenderId: "242031426628",
+  appId: "1:242031426628:web:5caa4640a7eb7e3576d011",
+  measurementId: "G-FG1ZHTHF7G"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
