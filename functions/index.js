ec8b5d2b34c91cf0f8cf9a8da3f6d351429ea75e
ycxpmmwnz2-create <ycxpmmwnz2@privaterelay.appleid.com>
2026-06-30 19:12:49 -0600

    Add Cloud Functions scaffold

---
+++ functions/index.js
@@ -0,0 +1,6 @@
+import { onCall } from 'firebase-functions/v2/https'
+
+export const studentPinLogin = onCall(() => ({
+  implemented: false,
+  message: 'Student PIN login is not implemented yet.',
+}))
